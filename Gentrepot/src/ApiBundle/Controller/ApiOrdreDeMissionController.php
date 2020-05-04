73b7b48b1faa2fdd7af8b2b0851bf7efe6151909
oumayma khenine <khenine.oumayma1@esprit.tn>
2020-05-04 06:37:27 +0100

    done

--- Gentrepot/src/ApiBundle/Controller/ApiOrdreDeMissionController.php
+++ Gentrepot/src/ApiBundle/Controller/ApiOrdreDeMissionController.php
@@ -2,8 +2,12 @@
 
 namespace ApiBundle\Controller;
 
+use logistiqueBundle\Entity\ordremission;
+use logistiqueBundle\Form\ordremissionType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
@@ -16,4 +20,20 @@ class ApiOrdreDeMissionController extends Controller
         $formatted = $serializer->normalize($ordrem);
         return new JsonResponse($formatted);
     }
+
+    public function ajoutordreMAction(Request $request){
+        $ordre= new ordremission();
+        $ordre->setId($request->get('id'));
+        $ordre->setIdChauffeur($request->get('cin'));
+        $ordre->setIdAidechauff($request->get('cin'));
+        $ordre->setIdVehicule($request->get('matricule'));
+        $ordre->setBondelivraisons($request->get('bondelivraison'));
+
+            $em= $this->getDoctrine()->getManager();
+            $em-> persist($ordre);
+            $em->flush();
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($ordre);
+        return new JsonResponse($formatted);
+        }
 }
