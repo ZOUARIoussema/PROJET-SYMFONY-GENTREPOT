463474df47e92d7b3a5c68183b4271687fdd4503
Nadia Cheikh <60850329+nadiacheikh@users.noreply.github.com>
2020-05-26 12:46:02 +0200

    les contrôleurs

--- Gentrepot/src/ApiBundle/Controller/ApiFournisseurController.php
+++ Gentrepot/src/ApiBundle/Controller/ApiFournisseurController.php
@@ -2,10 +2,17 @@
 
 namespace ApiBundle\Controller;
 
+use AchatBundle\Entity\BonEntree;
+use AchatBundle\Entity\BonRetour;
+use AchatBundle\Entity\Fournisseur;
 use Doctrine\Common\Collections\ArrayCollection;
+use StockageBundle\Entity\CommandeDAprovisionnement;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use TresorerieBundle\Entity\LettreDeRelance;
@@ -14,7 +21,70 @@ use UserBundle\Entity\User;
 
 class ApiFournisseurController extends Controller
 {
+    public function deletefournisseurAction($id){
+        $produit = $this->getDoctrine()->getRepository(Fournisseur::class)->find($id);
+        $souscat = $this->getDoctrine()->getRepository(CommandeDAprovisionnement::class)->findBy(array("fournisseur"=>$produit));
+        $em = $this->getDoctrine()->getManager();
+        foreach ($souscat as $s){
+            $b = $this->getDoctrine()->getRepository(BonEntree::class)->findOneBy(array('numeroC_commandeAp'=>$s));
+            $c = $this->getDoctrine()->getRepository(BonRetour::class)->findOneBy(array('numeroC_commandeAp'=>$s));
+            $em->remove($b);
+            $em->remove($c);
+            $em->remove($s);
+            $em->flush();
+        }
 
+        $em->remove($produit);
+        $em->flush();
+        return new Response("Done");
+
+
+    }
+
+    public function editFournisseurAction(Request $request){
+        $fournisseur = $this->getDoctrine()->getRepository(Fournisseur::class)->find($request->get('id'));
+        $fournisseur->setRaisonSociale($request->get('raisonSociale'));
+        $fournisseur->setNumeroTelephone($request->get('numeroTelephone'));
+        $fournisseur->setAdresse($request->get('adresse'));
+        $fournisseur->setAdresseMail($request->get('adresseMail'));
+        $fournisseur->setMatriculeFiscale($request->get('matriculeFiscale'));
+        $fournisseur->setCodePostale($request->get('codePostale'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($fournisseur);
+        $em->flush();
+        return new Response("Done");
+
+    }
+
+    public function AddFournisseurAction(Request $request){
+        $fournisseur = new Fournisseur();
+        $fournisseur->setRaisonSociale($request->get('raisonSociale'));
+        $fournisseur->setNumeroTelephone($request->get('numeroTelephone'));
+        $fournisseur->setAdresse($request->get('adresse'));
+        $fournisseur->setAdresseMail($request->get('adresseMail'));
+        $fournisseur->setMatriculeFiscale($request->get('matriculeFiscale'));
+        $fournisseur->setCodePostale($request->get('codePostale'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($fournisseur);
+        $em->flush();
+        return new Response("Done");
+    }
+
+    public function listFounisseurAction(){
+        $fournisseur = $this->getDoctrine()->getRepository(Fournisseur::class)->findAll();
+
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(2);
+// Add Circular reference handler
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        $normalizers = array($normalizer);
+        $serializer = new Serializer($normalizers, $encoders);
+        $formatted = $serializer->normalize(['fournisseur' => $fournisseur]);
+        return new JsonResponse($formatted);
+    }
 
 
 
--- Gentrepot/src/ApiBundle/Controller/ApiProduitController.php
+++ Gentrepot/src/ApiBundle/Controller/ApiProduitController.php
@@ -3,8 +3,13 @@
 namespace ApiBundle\Controller;
 
 use AchatBundle\Entity\ProduitAchat;
+use AchatBundle\Entity\SousCategorieAchat;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 use VenteBundle\Entity\Produit;
@@ -20,6 +25,84 @@ class ApiProduitController extends Controller
             ->findAll();
         $serializer = new Serializer([new ObjectNormalizer()]);
         $formatted = $serializer->normalize($tasks);
+        return new JsonResponse($formatted);
+    }
+
+    public function deleteProductAction($id){
+        $produit = $this->getDoctrine()->getRepository(ProduitAchat::class)->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($produit);
+        $em->flush();
+        return new Response("Done");
+
+
+    }
+
+    public function editProductAction(Request $request){
+        $produit = $this->getDoctrine()->getRepository(ProduitAchat::class)->find($request->get('ref'));
+        $sousCat = $this->getDoctrine()->getRepository(SousCategorieAchat::class)->findOneBy(array('name'=>$request->get('sousCat')));
+        $produit->setReference($request->get('ref'));
+        $produit->setLibelle($request->get('lib'));
+        $produit->setQuantiteEnStock($request->get('quantiteEnStock'));
+        $produit->setClasse($request->get('classe'));
+        $produit->setQuantiteStockSecurite($request->get('quantiteStockSecurite'));
+        $produit->setDernierPrixAchat($request->get('dernierPrixAchat'));
+        $produit->setTVA($request->get('TVA'));
+        $produit->setDimension($request->get('dimension'));
+        $produit->setDescription($request->get('description'));
+        $produit->setTypeDeConditionnement($request->get('typeDeConditionnement'));
+        $produit->setPrixVente($request->get('prixVente'));
+        $produit->setSousCategorie($sousCat);
+        $produit->setImage($request->get('image'));
+        $produit->setImage1($request->get('image'));
+        $produit->setImage2($request->get('image'));
+        $produit->setImage3($request->get('image'));
+        $produit->setImage4($request->get('image'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($produit);
+        $em->flush();
+        return new Response("Done");
+    }
+
+
+    public function AddProductAction(Request $request){
+        $produit = new ProduitAchat();
+        $sousCat = $this->getDoctrine()->getRepository(SousCategorieAchat::class)->findOneBy(array('name'=>$request->get('sousCat')));
+        $produit->setReference($request->get('ref'));
+        $produit->setLibelle($request->get('lib'));
+        $produit->setQuantiteEnStock($request->get('quantiteEnStock'));
+        $produit->setClasse($request->get('classe'));
+        $produit->setQuantiteStockSecurite($request->get('quantiteStockSecurite'));
+        $produit->setDernierPrixAchat($request->get('dernierPrixAchat'));
+        $produit->setTVA($request->get('TVA'));
+        $produit->setDimension($request->get('dimension'));
+        $produit->setDescription($request->get('description'));
+        $produit->setTypeDeConditionnement($request->get('typeDeConditionnement'));
+        $produit->setPrixVente($request->get('prixVente'));
+        $produit->setSousCategorie($sousCat);
+        $produit->setImage($request->get('image'));
+        $produit->setImage1($request->get('image'));
+        $produit->setImage2($request->get('image'));
+        $produit->setImage3($request->get('image'));
+        $produit->setImage4($request->get('image'));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($produit);
+        $em->flush();
+        return new Response("Done");
+    }
+
+    public function ListProductAction(){
+        $produits = $this->getDoctrine()->getRepository(ProduitAchat::class)->findAll();
+        $encoders = array(new XmlEncoder(), new JsonEncoder());
+        $normalizer = new ObjectNormalizer();
+        $normalizer->setCircularReferenceLimit(2);
+// Add Circular reference handler
+        $normalizer->setCircularReferenceHandler(function ($object) {
+            return $object->getId();
+        });
+        $normalizers = array($normalizer);
+        $serializer = new Serializer($normalizers, $encoders);
+        $formatted = $serializer->normalize(['produit' => $produits]);
         return new JsonResponse($formatted);
     }
 }
