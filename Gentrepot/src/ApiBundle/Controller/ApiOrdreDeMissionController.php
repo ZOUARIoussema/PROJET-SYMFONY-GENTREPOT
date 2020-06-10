02d66c2a12e9b2a2d1838aa7cbbdf1aec6707c7d
oumayma khenine <khenine.oumayma1@esprit.tn>
2020-06-10 14:34:49 +0100

    update

--- Gentrepot/src/ApiBundle/Controller/ApiOrdreDeMissionController.php
+++ Gentrepot/src/ApiBundle/Controller/ApiOrdreDeMissionController.php
@@ -4,12 +4,14 @@ namespace ApiBundle\Controller;
 
 use logistiqueBundle\Entity\ordremission;
 use logistiqueBundle\Form\ordremissionType;
+use Proxies\__CG__\logistiqueBundle\Entity\chauffeur;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class ApiOrdreDeMissionController extends Controller
 {
@@ -22,14 +24,32 @@ class ApiOrdreDeMissionController extends Controller
     }
 
     public function ajoutordreMAction(Request $request){
+
+
+
         $ordre= new ordremission();
+
         $ordre->setId($request->get('id'));
-        $ordre->setIdChauffeur($request->get('cin'));
-        $ordre->setIdAidechauff($request->get('cin'));
-        $ordre->setIdVehicule($request->get('matricule'));
+        $ordre->setIdChauffeur($this->getDoctrine()->getRepository(\logistiqueBundle\Entity\chauffeur::class)->find($request->get('chauffeur')));
+        $ordre->setIdAidechauff($this->getDoctrine()->getRepository(\logistiqueBundle\Entity\aidechauffeur::class)->find($request->get('Aidechauffeur')));
+        $ordre->setIdVehicule($this->getDoctrine()->getRepository(\logistiqueBundle\Entity\vehicule::class)->find($request->get('vehicule')));
         $ordre->setBondelivraisons($request->get('bondelivraison'));
+
+        $ordre->setDatecreation(new \DateTime());
+
+
+
+        $ordre->setDatesortie( new \DateTime($request->get('dateSortie')));
+        $ordre->setDateretour(new \DateTime($request->get('dateRetour')));
+
+
         $ordre->getIdVehicule()->setEtat('non disponible');
-        $ordre->getIdChauffeur()->setEtat('non disponible');
+       $ordre->getIdChauffeur()->setEtat('non disponible');
+
+
+
+
+
         $nb=$ordre->getIdChauffeur()->getVoyage();
         $ordre->getIdChauffeur()->setVoyage($nb+1);
             $em= $this->getDoctrine()->getManager();
